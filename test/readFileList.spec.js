cf203ce66c06b0184d0482930ad646d2043c8039
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-22 03:39:05 +0900

    remove tests

--- test/readFileList.spec.js
+++
@@ -1,9 +0,0 @@
-import readMonthFileList from "../scripts/month";
-
-test("This directory doesn't exists.", () => {
-  expect(readMonthFileList("2019", "13")).toEqual([]);
-});
-
-test("Directory exists, files are not empty.", () => {
-  expect(readMonthFileList("2019", "01").length).not.toBe(0);
-});
