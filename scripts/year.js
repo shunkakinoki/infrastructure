57c4b5a2bc5752587329e87ac17f728176f60db7
Shun Kakinoki <shunkakinoki@gmail.com>
2019-10-22 00:06:08 +0900

    add initial scripts

---
+++ scripts/year.js
@@ -0,0 +1,29 @@
+const {
+  resolve,
+} = require("path");
+const {
+  existsSync,
+  lstatSync,
+  readdirSync,
+} = require("fs");
+
+function readYearFileList(year) {
+  const fileList = [];
+  const dirPath = resolve(`./docs/year/${year}`);
+  const isDir = existsSync(dirPath) && lstatSync(dirPath).isDirectory();
+  if (!isDir) {
+    return fileList;
+  }
+
+  const files = readdirSync(dirPath);
+  files.forEach((item) => {
+    const currentFile = item.slice(0, 4);
+    fileList.push([
+      `/year/${currentFile}`,
+      `${currentFile}`,
+    ]);
+  });
+  return fileList.reverse();
+}
+
+module.exports = readYearFileList;
