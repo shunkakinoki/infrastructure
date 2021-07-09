69960e29bc7458aac29a52d0eae49f024452bedf
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-09 16:37:19 +0900

    chore: update

--- github/repo/config.tf
+++ github/repo/config.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "config" {
-  name           = "config"
+  name           = "configurations"
   description    = "My Personal Configurations :computer::bulb::memo:"
   default_branch = "master"
 
