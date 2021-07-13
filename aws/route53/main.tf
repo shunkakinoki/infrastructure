b3e90f6bbbf79ca31f9b571e9be80754f401a254
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-13 14:25:42 +0900

    chore: updae

--- aws/route53/main.tf
+++ aws/route53/main.tf
@@ -31,12 +31,20 @@ resource "aws_route53_record" "shunkakinoki_com_TXT" {
   ttl     = "300"
 }
 
+data "aws_s3_bucket" "shunkakinoki_com" {
+  bucket = "shunkakinoki.com"
+}
+
 resource "aws_route53_record" "w_shunkakinoki_com_A" {
   zone_id = aws_route53_zone.shunkakinoki.zone_id
   name    = "w.${aws_route53_zone.shunkakinoki.name}"
   type    = "A"
-  records = ["shunkakinoki.com.s3-website-us-east-1.amazonaws.com"]
-  ttl     = "300"
+
+  alias {
+    name                   = data.aws_s3_bucket.shunkakinoki_com.website_domain
+    zone_id                = data.aws_s3_bucket.shunkakinoki_com.hosted_zone_id
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "www_shunkakinoki_com_CNAME" {
