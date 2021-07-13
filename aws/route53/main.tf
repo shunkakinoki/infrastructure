6b6ee03ba13345c5bfa5d74f5223033303411d78
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-13 13:05:25 +0900

    perf: ini newsletter subdomain

--- aws/route53/main.tf
+++ aws/route53/main.tf
@@ -23,51 +23,11 @@ resource "aws_route53_record" "shunkakinoki_com_A" {
   ttl     = "300"
 }
 
-resource "aws_route53_record" "shunkakinoki_com_MX_A" {
-  zone_id = aws_route53_zone.shunkakinoki.zone_id
-  name    = aws_route53_zone.shunkakinoki.name
-  type    = "MX"
-  records = ["mxa.mailgun.org"]
-  ttl     = "300"
-}
-
-resource "aws_route53_record" "shunkakinoki_com_MX_B" {
-  zone_id = aws_route53_zone.shunkakinoki.zone_id
-  name    = aws_route53_zone.shunkakinoki.name
-  type    = "MX"
-  records = ["mxb.mailgun.org"]
-  ttl     = "300"
-}
-
 resource "aws_route53_record" "shunkakinoki_com_TXT" {
   zone_id = aws_route53_zone.shunkakinoki.zone_id
   name    = aws_route53_zone.shunkakinoki.name
   type    = "TXT"
-  records = ["v=spf1 include:mailgun.org ~all"]
-  ttl     = "300"
-}
-
-# resource "aws_route53_record" "shunkakinoki_com_TXT" {
-#   zone_id = aws_route53_zone.shunkakinoki.zone_id
-#   name    = aws_route53_zone.shunkakinoki.name
-#   type    = "TXT"
-#   records = ["keybase-site-verification=o1E1Em_NqWB-miaYh0dAkPwdjYPU2AZj2XYw_2fqkAU"]
-#   ttl     = "300"
-# }
-
-resource "aws_route53_record" "pic__domainkey_shunkakinoki_com_TXT" {
-  zone_id = aws_route53_zone.shunkakinoki.zone_id
-  name    = "pic._domainkey.${aws_route53_zone.shunkakinoki.name}"
-  type    = "TXT"
-  records = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9dcj1hl4V2kmkZ/bSWM9kO38X1DuI8/K5yvHRNpyPbmnU3UKMSepa3aTW6QrJzj4/7aDZDPTS6QlSCEMj+tILL+8PyQCabYkhPHCQzNOiQOozkOm4opK/OoMRib+OMWUNJ/5/PPTCjicZYVCmmeqoTMOhmxceR2yltQGOmSHAQwIDAQAB"]
-  ttl     = "300"
-}
-
-resource "aws_route53_record" "email_shunkakinoki_com_CNAME" {
-  zone_id = aws_route53_zone.shunkakinoki.zone_id
-  name    = "email.${aws_route53_zone.shunkakinoki.name}"
-  type    = "CNAME"
-  records = ["mailgun.org"]
+  records = ["keybase-site-verification=o1E1Em_NqWB-miaYh0dAkPwdjYPU2AZj2XYw_2fqkAU"]
   ttl     = "300"
 }
 
@@ -76,5 +36,37 @@ resource "aws_route53_record" "www_shunkakinoki_com_CNAME" {
   name    = "www.${aws_route53_zone.shunkakinoki.name}"
   type    = "CNAME"
   records = ["cname.vercel-dns.com"]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "shunkakinoki_com_MX" {
+  zone_id = aws_route53_zone.shunkakinoki.zone_id
+  name    = "newsletter.${aws_route53_zone.shunkakinoki.name}"
+  type    = "MX"
+  records = ["mxa.mailgun.org", "mxb.mailgun.org"]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "newsletter_shunkakinoki_com_TXT" {
+  zone_id = aws_route53_zone.shunkakinoki.zone_id
+  name    = "newsletter.${aws_route53_zone.shunkakinoki.name}"
+  type    = "TXT"
+  records = ["v=spf1 include:mailgun.org ~all"]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "email_newsletter_shunkakinoki_com_CNAME" {
+  zone_id = aws_route53_zone.shunkakinoki.zone_id
+  name    = "email.newsletter.${aws_route53_zone.shunkakinoki.name}"
+  type    = "CNAME"
+  records = ["mailgun.org"]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "pic__domainkey_shunkakinoki_com_TXT" {
+  zone_id = aws_route53_zone.shunkakinoki.zone_id
+  name    = "mx._domainkey.newsletter.${aws_route53_zone.shunkakinoki.name}"
+  type    = "TXT"
+  records = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC9dcj1hl4V2kmkZ/bSWM9kO38X1DuI8/K5yvHRNpyPbmnU3UKMSepa3aTW6QrJzj4/7aDZDPTS6QlSCEMj+tILL+8PyQCabYkhPHCQzNOiQOozkOm4opK/OoMRib+OMWUNJ/5/PPTCjicZYVCmmeqoTMOhmxceR2yltQGOmSHAQwIDAQAB"]
   ttl     = "300"
 }
