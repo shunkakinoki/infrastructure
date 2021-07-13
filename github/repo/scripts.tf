e5e6091f188c3423689d208004f0a577f638e1fe
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-13 17:03:16 +0900

    chore: update

---
+++ github/repo/scripts.tf
@@ -0,0 +1,19 @@
+resource "github_repository" "scripts" {
+  name           = "scripts"
+  description    = "My Personal Scripts :notebook::whale::partly_sunny:"
+  homepage_url   = ""
+  default_branch = "main"
+
+  archived               = false
+  visibility             = "public"
+  has_issues             = true
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  allow_merge_commit     = true
+  allow_squash_merge     = true
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+
+  topics = ["docker", "personal", "scripts"]
+}
