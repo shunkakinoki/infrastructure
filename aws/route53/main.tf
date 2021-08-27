0551eee312cb6b294c955127e08cbfb3ea49d5c8
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-26 18:08:11 -0700

    chore: update

--- aws/route53/main.tf
+++ aws/route53/main.tf
@@ -27,7 +27,7 @@ resource "aws_route53_record" "shunkakinoki_com_TXT" {
   zone_id = aws_route53_zone.shunkakinoki.zone_id
   name    = aws_route53_zone.shunkakinoki.name
   type    = "TXT"
-  records = ["a=0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed"]
+  records = ["keybase-site-verification=o1E1Em_NqWB-miaYh0dAkPwdjYPU2AZj2XYw_2fqkAU"]
   ttl     = "300"
 }
 
@@ -80,5 +80,13 @@ resource "aws_route53_record" "mx__domainkey_newsletter_shunkakinoki_com_TXT" {
   name    = "mx._domainkey.newsletter.${aws_route53_zone.shunkakinoki.name}"
   type    = "TXT"
   records = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCu+a07Jb7uXs/ekmrFZUH71gPQMHe3EbW+zYI+QKAWh7bK5B0uTUJ5PxFJ9nurNPZ7FdJboaF/WQmJEwzLATfcYHXJr1o/xh9NQH3jf62B6CRWAB6oxKdAflZwn6rv8xBLxuL2NKQmDMfOY6zbNG68wI4s7T+ocNMoKYArePgvBwIDAQAB"]
+  ttl     = "300"
+}
+
+resource "aws_route53_record" "_ens_shunkakinoki_com_TXT" {
+  zone_id = aws_route53_zone.shunkakinoki.zone_id
+  name    = "_ens.${aws_route53_zone.shunkakinoki.name}"
+  type    = "TXT"
+  records = ["a=0x4fd9D0eE6D6564E80A9Ee00c0163fC952d0A45Ed"]
   ttl     = "300"
 }
