084741af8bf3b9a649b3ee62f9921d5c34e82351
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-30 00:37:04 -0700

    chore: update

--- environments/main/variables.tf
+++ environments/main/variables.tf
@@ -5,3 +5,15 @@ variable "slack_aws_cost" {
 variable "slack_aws_cost_log" {
   type = string
 }
+
+variable "ACCESS_TOKEN" {
+  type = string
+}
+
+variable "NPM_TOKEN" {
+  type = string
+}
+
+variable "NOTION_API_KEY" {
+  type = string
+}
