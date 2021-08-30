ced9d585dc6a8fe628bc3f569e8528c78ed065cd
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-30 11:01:53 -0700

    chore: update

--- aws/route53/main.tf
+++ aws/route53/main.tf
@@ -15,14 +15,6 @@ resource "aws_route53_record" "shunkakinoki_com" {
   ttl = "300"
 }
 
-resource "aws_route53_record" "shunkakinoki_com_A" {
-  zone_id = aws_route53_zone.shunkakinoki_com.zone_id
-  name    = aws_route53_zone.shunkakinoki_com.name
-  type    = "A"
-  records = ["76.76.21.21"]
-  ttl     = "300"
-}
-
 resource "aws_route53_record" "shunkakinoki_com_TXT" {
   zone_id = aws_route53_zone.shunkakinoki_com.zone_id
   name    = aws_route53_zone.shunkakinoki_com.name
@@ -33,22 +25,6 @@ resource "aws_route53_record" "shunkakinoki_com_TXT" {
 
 data "aws_s3_bucket" "shunkakinoki_com" {
   bucket = "shunkakinoki.com"
-}
-
-resource "aws_route53_record" "www_shunkakinoki_com_CNAME" {
-  zone_id = aws_route53_zone.shunkakinoki_com.zone_id
-  name    = "www.${aws_route53_zone.shunkakinoki_com.name}"
-  type    = "CNAME"
-  records = ["cname.vercel-dns.com"]
-  ttl     = "300"
-}
-
-resource "aws_route53_record" "og_shunkakinoki_com_CNAME" {
-  zone_id = aws_route53_zone.shunkakinoki_com.zone_id
-  name    = "og.${aws_route53_zone.shunkakinoki_com.name}"
-  type    = "CNAME"
-  records = ["cname.vercel-dns.com"]
-  ttl     = "300"
 }
 
 resource "aws_route53_record" "storybook_shunkakinoki_com_CNAME" {
