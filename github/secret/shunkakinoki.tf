f32b02827dccab2e428b59006e33ee6a23da8ebd
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-30 00:18:38 -0700

    chore: update

--- github/secret/shunkakinoki.tf
+++ github/secret/shunkakinoki.tf
@@ -1,6 +1,6 @@
-# data "aws_iam_user" "aws" {
-#   user_name = "github"
-# }
+data "aws_iam_user" "aws" {
+  user_name = "github"
+}
 
 data "aws_caller_identity" "aws" {}
 
@@ -8,49 +8,81 @@ data "github_repository" "shunkakinoki" {
   full_name = "shunkakinoki/shunkakinoki"
 }
 
-# resource "aws_iam_access_key" "aws" {
-#   user = data.aws_iam_user.aws.user_name
-# }
+resource "aws_iam_access_key" "aws" {
+  user = data.aws_iam_user.aws.user_name
+}
 
 resource "github_actions_secret" "ACCESS_TOKEN" {
   repository  = data.github_repository.shunkakinoki.id
   secret_name = "ACCESS_TOKEN"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "AWS_ACCESS_KEY_ID" {
-  repository  = data.github_repository.shunkakinoki.id
-  secret_name = "AWS_ACCESS_KEY_ID"
-  # plaintext_value = aws_iam_access_key.aws.id
+  repository      = data.github_repository.shunkakinoki.id
+  secret_name     = "AWS_ACCESS_KEY_ID"
+  plaintext_value = aws_iam_access_key.aws.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "AWS_ACCOUNT_NUMBER" {
   repository      = data.github_repository.shunkakinoki.id
   secret_name     = "AWS_ACCOUNT_NUMBER"
   plaintext_value = data.aws_caller_identity.aws.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "AWS_SECRET_ACCESS_KEY" {
   repository  = data.github_repository.shunkakinoki.id
   secret_name = "AWS_SECRET_ACCESS_KEY"
   # plaintext_value = aws_iam_access_key.aws.secret
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "CARGO_REGISTRY_TOKEN" {
   repository  = data.github_repository.shunkakinoki.id
   secret_name = "CARGO_REGISTRY_TOKEN"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "DOCKER_PASSWORD" {
   repository  = data.github_repository.shunkakinoki.id
   secret_name = "DOCKER_PASSWORD"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "NOTION_API_KEY" {
   repository  = data.github_repository.shunkakinoki.id
   secret_name = "NOTION_API_KEY"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "github_actions_secret" "NPM_TOKEN" {
   repository  = data.github_repository.shunkakinoki.id
   secret_name = "NPM_TOKEN"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
