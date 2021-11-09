e2da927a903030bceabcffa3eb0089ec8da35dad
Shun Kakinoki <shunkakinoki@gmail.com>
2021-11-09 16:19:08 -0500

    chore: update

--- aws/route53/0xkaki_xyz.tf
+++ aws/route53/0xkaki_xyz.tf
@@ -1,32 +1,32 @@
-resource "aws_route53_zone" "0xkaki_xyz" {
+resource "aws_route53_zone" "_0xkaki_xyz" {
   name = "0xkaki.xyz"
 }
 
-resource "aws_route53_record" "0xkaki_xyz" {
-  zone_id = aws_route53_zone["0xkaki_xyz"].zone_id
-  name    = aws_route53_zone["0xkaki_xyz"].name
+resource "aws_route53_record" "_0xkaki_xyz" {
+  zone_id = aws_route53_zone._0xkaki_xyz.zone_id
+  name    = aws_route53_zone._0xkaki_xyz.name
   type    = "NS"
   records = [
-    aws_route53_zone["0xkaki_xyz"].name_servers.0,
-    aws_route53_zone["0xkaki_xyz"].name_servers.1,
-    aws_route53_zone["0xkaki_xyz"].name_servers.2,
-    aws_route53_zone["0xkaki_xyz"].name_servers.3,
+    aws_route53_zone._0xkaki_xyz.name_servers.0,
+    aws_route53_zone._0xkaki_xyz.name_servers.1,
+    aws_route53_zone._0xkaki_xyz.name_servers.2,
+    aws_route53_zone._0xkaki_xyz.name_servers.3,
   ]
   ttl = "300"
 }
 
-resource "aws_route53_record" "0xkaki_xyz_A" {
-  zone_id = aws_route53_zone["0xkaki_xyz"].zone_id
-  name    = aws_route53_zone["0xkaki_xyz"].name
+resource "aws_route53_record" "_0xkaki_xyz_A" {
+  zone_id = aws_route53_zone._0xkaki_xyz.zone_id
+  name    = aws_route53_zone._0xkaki_xyz.name
   type    = "A"
   records = ["76.76.21.21"]
   ttl     = "300"
 }
 
-resource "aws_route53_record" "www_0xkaki_xyz_CNAME" {
-  zone_id = aws_route53_zone["0xkaki_xyz"].zone_id
-  name    = "www.${aws_route53_zone["0xkaki_xyz"].name}"
+resource "aws_route53_record" "www__0xkaki_xyz_CNAME" {
+  zone_id = aws_route53_zone._0xkaki_xyz.zone_id
+  name    = "www.${aws_route53_zone._0xkaki_xyz.name}"
   type    = "CNAME"
-  records = ["cname.vercel-dns.xyz"]
+  records = ["cname.vercel-dns.com"]
   ttl     = "300"
 }
