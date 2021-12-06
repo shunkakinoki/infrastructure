fc9aeb8c6fd312067bfe19992f1906c46f1c5ad3
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-12-06 17:33:56 -0500

    Update wagumi_xyz.tf

--- aws/route53/wagumi_xyz.tf
+++ aws/route53/wagumi_xyz.tf
@@ -33,6 +33,14 @@ resource "aws_route53_record" "www_wagumi_xyz_CNAME" {
   ttl     = "300"
 }
 
+resource "aws_route53_record" "airdrop_wagumi_xyz_CNAME" {
+  zone_id = aws_route53_zone.wagumi_xyz.zone_id
+  name    = "airdrop.${aws_route53_zone.wagumi_xyz.name}"
+  type    = "CNAME"
+  records = ["cname.vercel-dns.com"]
+  ttl     = "300"
+}
+
 resource "aws_route53_record" "cats_wagumi_xyz_CNAME" {
   zone_id = aws_route53_zone.wagumi_xyz.zone_id
   name    = "cats.${aws_route53_zone.wagumi_xyz.name}"
