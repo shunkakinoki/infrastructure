c103d1c96256dfba0c7de5e54a68250d872dbb7d
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-12-06 16:48:41 -0500

    Update wagumi_xyz.tf

--- aws/route53/wagumi_xyz.tf
+++ aws/route53/wagumi_xyz.tf
@@ -21,7 +21,6 @@ resource "aws_route53_record" "wagumi_xyz_A" {
   zone_id = aws_route53_zone.wagumi_xyz.zone_id
   name    = aws_route53_zone.wagumi_xyz.name
   type    = "A"
-  type    = "A"
   records = ["76.76.21.21"]
   ttl = "300"
 }
