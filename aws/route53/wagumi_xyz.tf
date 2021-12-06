3da4e2446e929558343a641de9697a8bc8b95fd0
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-12-06 14:56:03 -0500

    Update wagumi_xyz.tf

--- aws/route53/wagumi_xyz.tf
+++ aws/route53/wagumi_xyz.tf
@@ -21,12 +21,8 @@ resource "aws_route53_record" "wagumi_xyz_A" {
   zone_id = aws_route53_zone.wagumi_xyz.zone_id
   name    = aws_route53_zone.wagumi_xyz.name
   type    = "A"
-  records = [
-    "185.199.108.153",
-    "185.199.109.153",
-    "185.199.110.153",
-    "185.199.111.153",
-  ]
+  type    = "A"
+  records = ["76.76.21.21"]
   ttl = "300"
 }
 
@@ -34,6 +30,6 @@ resource "aws_route53_record" "www_wagumi_xyz_CNAME" {
   zone_id = aws_route53_zone.wagumi_xyz.zone_id
   name    = "www.${aws_route53_zone.wagumi_xyz.name}"
   type    = "CNAME"
-  records = ["wagumi.github.io"]
+  records = ["cname.vercel-dns.com"]
   ttl     = "300"
 }
