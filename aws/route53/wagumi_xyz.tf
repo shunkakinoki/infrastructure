bdb92ad04864a7c82d46a42fded0fc838eaf40af
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2022-01-03 14:38:27 -0800

    Update wagumi_xyz.tf

--- aws/route53/wagumi_xyz.tf
+++ aws/route53/wagumi_xyz.tf
@@ -49,9 +49,9 @@ resource "aws_route53_record" "cats_wagumi_xyz_CNAME" {
   ttl     = "300"
 }
 
-resource "aws_route53_record" "vote_wagumi_xyz_CNAME" {
+resource "aws_route53_record" "gov_wagumi_xyz_CNAME" {
   zone_id = aws_route53_zone.wagumi_xyz.zone_id
-  name    = "vote.${aws_route53_zone.wagumi_xyz.name}"
+  name    = "gov.${aws_route53_zone.wagumi_xyz.name}"
   type    = "CNAME"
   records = ["snapshotpage.b-cdn.net"]
   ttl     = "300"
